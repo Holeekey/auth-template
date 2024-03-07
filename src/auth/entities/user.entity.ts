844c9001d6ccf7a15459bb5fc7e415e13aeb7918
Holeekey <danbrunosv@gmail.com>
2024-03-07 14:22:22 -0400

    first_test

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,44 @@
+import { BeforeInsert, BeforeUpdate, Column, PrimaryGeneratedColumn } from 'typeorm';
+export class User {
+    
+    // @PrimaryGeneratedColumn('uuid')
+    // id: string;
+
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column('text', {
+        unique: true
+    })
+    email: string;
+
+    @Column('text', {
+        select: false
+    })
+    password: string;
+
+    @Column('text')
+    fullName: string;
+
+    @Column('bool', {
+        default: true
+    })
+    isActive: boolean;
+
+    @Column('text', {
+        array: true,
+        default: ['user']
+    })
+    roles: string[];
+
+    @BeforeInsert()
+    checkFieldsBeforeInsert() {
+        this.email = this.email.toLowerCase().trim();
+    }
+
+    @BeforeUpdate()
+    checkFieldsBeforeUpdate() {
+        this.checkFieldsBeforeInsert();   
+    }
+
+}
---
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -0,0 +1,7 @@
+
+
+export interface JwtPayload {
+    //id:string;
+
+    id:number;
+}
